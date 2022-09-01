7c0da738d14cac6af66f0759b711e85b56ba1d5c
samuel.imex@gmail.com <samuel.imex@gmail.com>
2022-09-01 15:18:51 +0100

    updated server.js file

--- server.js
+++ server.js
@@ -81,8 +81,11 @@ app.get("/", (req, res) => {
     user.forEach(o => console.log(o.email_address));
     
     cron.schedule('*/1 * * * *', () => {
-        console.log('running every minute to ');
-        message.forEach(o => console.log(o));
+        console.log('running every minute');
+        message.forEach(o => {
+            console.log(o)
+            res.send(o)
+        });
 
   });
 
